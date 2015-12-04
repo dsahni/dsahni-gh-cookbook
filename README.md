357d7e53b6db4cd13030cd36d76950deb0fc09cb
Dhruv Sahni <dhruvsahni@gmail.com>
2015-12-04 12:59:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# dsahni-gh-cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'dsahni-gh-cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures dsahni-gh-cookbook'
+long_description 'Installs/Configures dsahni-gh-cookbook'
+version '0.1.0'
